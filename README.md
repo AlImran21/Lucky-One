5ed1e8a78d30d0f6d4cfab948c2d0f5cd9e2e57f
Al-Imran <alimran21feb@gmail.com>
2022-03-26 12:57:59 +0600

    updated readme file

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Getting Started with Create React App
+# Guiter's Store
+## Click here to visit live site link [netlify.com](https://my-assignment8-with-react.netlify.app/)
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Header.css'
 const Header = () => {
     return (
-        <div>
-            <h1 style={{color: '#025205'}}>Guiter's Store</h1>
-            <h3 style={{color: '#6B2202', fontSize: '25px'}}>Take whatever you like</h3>
+        <div className='header'>
+            <h1 className='header-title' style={{color: '#025205'}}>Guiter's Store</h1>
+            <h3 className='mini-header' style={{color: '#6B2202', fontSize: '25px'}}>Take whatever you like</h3>
         </div>
     );
 };
